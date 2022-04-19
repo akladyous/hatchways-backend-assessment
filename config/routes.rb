07827398a05c383dfe0d4a85b8218630c6893fe3
Boula Akladyous <akladyous@gmail.com>
2022-04-19 19:15:32 -0400

    added required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    Rails.application.routes.draw do
+        namespace :api do
+            get "posts", to: "posts#show"
+            get "ping", to: "posts#ping"
+        end
+    end
 end
